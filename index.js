42197e0a2aa81f2ee7033388e5d6722c9ff1a0ab
Arnold <mainaarnold8350@gmail.com>
2025-06-27 11:18:55 +0300

    Complete DOM manipulation lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the main#main element
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element and assign it to newHeader
+const newHeader = document.createElement("h1");
+
+// Set an ID on the new header
+newHeader.id = "victory";
+
+// Set the inner HTML/textContent to show your name as the champion
+newHeader.textContent = "YOUR-NAME is the champion"; // <- replace YOUR-NAME with your actual name
